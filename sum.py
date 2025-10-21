707dff44bd1bd680200cee77e4fcc6720da2c8fe
柴田浩貴 <hiroki@shibatahiroshitakanoMacBook-Air.local>
2025-10-21 19:49:21 +0900

    sum

--- sum.py
+++ sum.py
@@ -8,3 +8,7 @@ from datetime import timedelta
 sample_list = [timedelta(days=2, hours=3), timedelta(hours=5, minutes=30), timedelta(days=1, minutes=45)]
 total_sum = sum(sample_list, timedelta())
 print(f"The sum of the list is: {total_sum}")
+
+print("-----")
+
+total_sum = sum(sample_list)
