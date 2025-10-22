0fd1f5c9a939bd4347eb375f06679d7c9274df9a
柴田浩貴 <hiroki@shibatahiroshitakanoMacBook-Air.local>
2025-10-22 16:48:49 +0900

    【Python】//と%で秒数を時間と分に変換する

--- total_seconds.py
+++ total_seconds.py
@@ -5,5 +5,10 @@ epoch = datetime(1970, 1, 1)
 
 total_seconds = (now - epoch).total_seconds()
 
-print("現在の日時:", now)
-print("エポックからの秒数:", total_seconds)
+# print("現在の日時:", now)
+# print("エポックからの秒数:", total_seconds)
+
+hours = total_seconds // 3600
+minutes = (total_seconds % 3600) // 60
+seconds = total_seconds % 60
+print(f"エポックからの時間: {int(hours)}時間 {int(minutes)}分 {int(seconds)}秒")
